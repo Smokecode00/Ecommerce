009be2ef9fec87a33616d975ae60e057362af594
SmokeCode00 <smokefacebook02@gmail.com>
2023-12-21 22:35:07 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,3 +1,40 @@
 <?php
 include 'navbar.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Cart</title>
+</head>
+<body>
+    <div class="container mt-3">
+        <div class="card">
+            <table class="table table-bordered text-center ">
+                <thead class="table-secondary">
+                    <tr>
+                        <th>ID</th>
+                        <th>Product Name</th>
+                        <th>Product Description</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Total Ammount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Keyboard</td>
+                        <td>Mechanical Blue Switch</td>
+                        <td>1</td>
+                        <td>2,000</td>
+                        <td>2,000</td>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
