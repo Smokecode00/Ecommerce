b0b2ee634b9e3e54e0928c99f2c2d74924c02d48
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2023-12-24 11:54:13 +0800

    adding some backend

--- login.php
+++ login.php
@@ -1,83 +1,104 @@
- <!DOCTYPE html>
- <html lang="en">
+<?php
+include 'Includes/dbconn.php';
+session_start();
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $result = mysqli_query($conn, "SELECT * FROM registertbl WHERE username= '$email' OR email = '$email'");
+    $row = mysqli_fetch_assoc($result);
+    if (mysqli_num_rows($result)) {
+        if ($password == $row['password']) {
+            $_SESSION['login'] = true;
+            $_SESSION['id'] = $row['id'];
+            header('location: home.php');
+            exit();
+        }
+        $msg[] = "Password Incorrect";
+    } else {
+        $msg[] = "User not registered";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
 
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- Font Awesome -->
-     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
-     <!-- Google Fonts -->
-     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-     <!-- MDBootstrap -->
-     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.css" rel="stylesheet" />
-     <title>Login</title>
- </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDBootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.css" rel="stylesheet" />
+    <title>Login</title>
+</head>
 
- <body>
-     <div class="container-fluid vh-100 bg-secondary ">
-         <div class="container py-5 h-100">
-             <div class="row d-flex justify-content-center align-items-center h-100">
-                 <div class="col col-xl-10">
-                     <div class="card" style="border-radius: 1rem;">
-                         <div class="row g-0">
-                             <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                 <img src="Img/logo.jpg" alt="login form" class="img-fluid mt-0 ms-0 border-2" />
-                             </div>
-                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                                 <div class="card-body p-4 p-lg-5 text-black">
+<body>
+    <div class="container-fluid vh-100 bg-secondary ">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-xl-10">
+                    <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="Img/logo.jpg" alt="login form" class="img-fluid mt-0 ms-0 border-2" />
+                            </div>
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
 
-                                     <form action="" method="post">
-                                         <div class="d-flex align-items-center mb-3 pb-1">
-                                             <img src="Img/logo.jpg" alt="logo" width="70" height="70" style="border-radius: 50%;">
-                                             <span class="h1 fw-bold mb-0">My Shop</span>
-                                         </div>
+                                    <form action="" method="post">
+                                        <div class="d-flex align-items-center mb-3 pb-1">
+                                            <img src="Img/logo.jpg" alt="logo" width="70" height="70" style="border-radius: 50%;">
+                                            <span class="h1 fw-bold mb-0">My Shop</span>
+                                        </div>
 
-                                         <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
 
-                                         <div class="form-outline mb-0">
-                                             <input type="text" id="usernameemail" name="usernameemail" class="form-control form-control-lg text-black fw-normal" />
-                                             <label class="form-label" for="usernameemail">Username or Email</label>
-                                         </div>
-                                         <!-- <?php
-                                                if (isset($error)) {
-                                                    foreach ($error as $error) {
+                                        <div class="form-outline mb-0">
+                                            <input type="text" id="usernameemail" name="usernameemail" class="form-control form-control-lg text-black fw-normal" />
+                                            <label class="form-label" for="usernameemail">Username or Email</label>
+                                        </div>
+                                        <!-- <?php
+                                                if (isset($msg)) {
+                                                    foreach ($msg as $error) {
                                                         echo '<span class="error-msg text-danger ms-2">' . $error . '</span>';
                                                     };
                                                 };
                                                 ?> -->
 
-                                         <div class="form-outline mb-0 mt-4">
-                                             <input type="password" id="password" name="password" class="form-control form-control-lg text-black" />
-                                             <label class="form-label" for="password">Password</label>
-                                         </div>
-                                         <!-- <?php
-                                                if (isset($error1)) {
-                                                    foreach ($error1 as $error1) {
+                                        <div class="form-outline mb-0 mt-4">
+                                            <input type="password" id="password" name="password" class="form-control form-control-lg text-black" />
+                                            <label class="form-label" for="password">Password</label>
+                                        </div>
+                                        <!-- <?php
+                                                if (isset($msg)) {
+                                                    foreach ($msg as $error1) {
                                                         echo '<span class="error-msg text-danger ms-2">' . $error1 . '</span>';
                                                     };
                                                 };
                                                 ?> -->
 
-                                         <div class="pt-1 mb-4 mt-4">
-                                             <input type="submit" name="submit" value="Login" class="btn btn-primary fw-semibold btn-lg btn-block rounded-5">
-                                         </div>
+                                        <div class="pt-1 mb-4 mt-4">
+                                            <input type="submit" name="submit" value="Login" class="btn btn-primary fw-semibold btn-lg btn-block rounded-5">
+                                        </div>
 
-                                         <p class="mb-5 pb-lg-2 text-black">Don't have an account? <a href="register.php" class="text-primary">Register here</a></p>
-                                     </form>
+                                        <p class="mb-5 pb-lg-2 text-black">Don't have an account? <a href="register.php" class="text-primary">Register here</a></p>
+                                    </form>
 
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
-     <!-- Bootstrap -->
-     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
- </body>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
+</body>
 
- </html>
- </body>
+</html>
+</body>
 
- </html>
+</html>
--- navbar.php
+++ navbar.php
@@ -5,13 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Navbar</title>
 </head>
 
 <body>
-    <nav class="navbar bg-secondary">
+    <nav class="navbar bg-secondary bg-opacity-50 ">
         <div class="container-fluid">
             <a href="#" class="navbar-brand fw-semibold">ECOMMERCE</a>
             <ul class="nav me-2">
@@ -25,8 +24,7 @@
                     <a href="cart.php" class="nav-link fw-semibold text-decoration-none text-black">My Cart</a>
                 </li>
                 <li class="nav-item dropdown me-5">
-                    <a class="nav-link dropdown-toggle text-black fw-semibold" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                    <a class="nav-link dropdown-toggle text-black fw-semibold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Setting
                     </a>
                     <ul class="dropdown-menu text-black">
@@ -43,8 +41,7 @@
         </div>
     </nav>
     <!-- JS Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 </body>
 
